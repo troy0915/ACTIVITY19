864d1ec6989a4f0eb12079f0e2f10039e3877b8b
troy0915 <johntroydolom7@gmail.com>
2025-08-08 22:57:28 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "open";
+            string userGuess;
+            do
+
+            {
+                // Prompt user for a guess
+                Console.Write("Enter your guess: ");
+                userGuess = Console.ReadLine();
+
+                // Check if the guess is correct (case-insensitive)
+                if (!string.Equals(userGuess, secretWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Try again");
+                }
+            } while (!string.Equals(userGuess, secretWord, StringComparison.OrdinalIgnoreCase)) ;
+
+            // If the guess is correct
+            Console.WriteLine("Correct!");
+        }
+    }
+}
+
+
+
